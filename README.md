58373de513c9fad06260b937b0d453340fc6d33b
Zachary Light <zach@vadweb.us>
2017-09-13 14:36:54 -0700

    uploaded required files, detailed readme to include useful information

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mercedClassAlert
 A bot that checks if there is space in a class and sends an email or text alert
+This program requires requets for python found at http://docs.python-requests.org
+
+
+HOW TO USE:
+First edit the classAlert.py file, and change email@gmail.com to a valid email, and password to that email's password. If an error occurs when run, the email may not allow unsafe apps to run using it, which can be changed at: https://myaccount.google.com/u/1/lesssecureapps
+
+Go to this website: https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.p_selectsubject
+and find the class you want an alert for. Find the subject code (i.e. CHEM, ENGR etc.) and the three didgit number
+Put your info in classList.txt in this layout:
+
+%
+N
+SUBJECT
+NUMBER	
+EMAIL
+
+continue to do this for all classes wanted, at the end of the file type:
+#END
+
+When this is done simply run the classAlert.py file
---
+++ classAlert.py
@@ -0,0 +1,56 @@
+import time
+import smtplib
+import requests
+
+
+
+skip = ''
+pos = 0
+code = ''
+num = ''
+email = ''
+cur = []
+
+with open('classList.txt') as f:
+	cur = f.read().splitlines()
+
+def nextPos(cPos):
+	global skip
+	global code
+	global num
+	global email
+        if '#END' in cur[cPos]:
+                cPos = 0
+	if 'SKIP' in cur[cPos+1]:
+		skip = 1
+	code = cur[cPos+2]
+	num = cur[cPos+3]
+	email = cur[cPos+4]
+	cPos = cPos+5
+	return cPos
+	
+pos = nextPos(pos)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("email@gmail.com", "password")
+while (1):
+	time.sleep(60)
+	url = 'https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.P_ViewSchedule?validterm=201730&subjcode='+code+'&openclasses=Y' #validterm will need to change based on term, could make part of text file, but easy enough to deal with
+	print(url)
+	r = requests.get(url)
+	check = "crsenumb="+num+"&"
+	print(check)
+	for string in r.text.split():
+		if check in string:
+			msg = "There is an opening in "+code+" "+num
+			print(code)
+			server.sendmail("email@gmail.com",email,msg)
+			print(email)
+			cur[pos-4] = 'SKIP'
+			break
+	pos = nextPos(pos)
+	if pos == 0:
+		time.sleep(60)
+server.quit()
+print('1')
