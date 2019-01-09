babdf2a131410ac1cdcf1ca28e69627fe8dded4c
Zachary Light <zacharylight15@mittymonarch.com>
2019-01-08 22:57:48 -0800

    added a system to create a list of all possible files in cleanUp, writes to a json file called classList.json

--- cleanUp.py
+++ cleanUp.py
@@ -1,8 +1,33 @@
 import json
+import requests
+import classAlert
 
-def write(jData):
-	with open("jsonLayout.json", "w") as r:
+def write(file, jData):
+	with open(file, "w") as r:
 		r.write(json.dumps(jData, sort_keys=True, indent=4, separators=(',', ':')))
+
+def populateTerm():
+    term = classAlert.checkTerm()
+    htmlData = requests.get('https://mystudentrecord.ucmerced.edu/pls/PROD/xhwschedule.P_ViewSchedule?validterm='+term+'&subjcode=ALL&openclasses=N')
+    arrayObj = {}
+    arrayObj["list"] = []
+    jsonObject = {}
+    for text in htmlData.text.split("&crsenumb="):
+        loc = text.split("=")
+        if jsonObject == {}:
+            jsonObject["subject"] = loc[len(loc)-1]
+            jsonObject["classes"] = []
+        else:
+            num = text.split("&")[0]
+            if num not in jsonObject["classes"]:
+                jsonObject["classes"].append(num)
+            if loc[len(loc)-1] != jsonObject["subject"]:
+                arrayObj["list"].append(jsonObject)
+                jsonObject = {}
+                jsonObject["subject"] = loc[len(loc)-1]
+                jsonObject["classes"] = []
+    write("classList.json", arrayObj)
+
 
 def cleanUp():
     deleteQueue = []
@@ -32,7 +57,7 @@ def cleanUp():
     for obj in deleteQueue:
         jsonData["classes"].remove(obj)
         count+=1
-    write(jsonData)
+    write("jsonLayout.json", jsonData)
     print "Ammount deleted: " + str(count)
 
 cleanUp()
