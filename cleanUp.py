655d73da465cf5fa1e90cb6ef301143909e6f8e4
Zachary Light <zacharylight15@mittymonarch.com>
2019-01-08 22:12:01 -0800

    realized the issue

--- cleanUp.py
+++ cleanUp.py
@@ -8,30 +8,31 @@ def cleanUp():
     deleteQueue = []
     with open("jsonLayout.json", "r") as r:
 	    jsonData = json.load(r)
+    count = 0
     for entry in jsonData["classes"]:
         if not entry["usesCRN"]:
             if len(entry["code"]) != 3:
                 deleteQueue.append(entry)
-                print found
                 continue
                 #checking for valid code should go here
         else:
             if len(entry["crn"])!=5:
                 deleteQueue.append(entry)
-                print found
                 continue
         if "@" not in entry["email"]:
             deleteQueue.append(entry)
-            print found
             continue
         if "." not in entry["email"]:
             deleteQueue.append(entry)
-            print found
             continue
         if entry["sent"]:
             deleteQueue.append(entry)
-            print found
             continue
+    print "Amount to be deleted: " + str(len(deleteQueue))
     for obj in deleteQueue:
         jsonData["classes"].remove(obj)
+        count+=1
     write(jsonData)
+    print "Ammount deleted: " + str(count)
+
+cleanUp()
