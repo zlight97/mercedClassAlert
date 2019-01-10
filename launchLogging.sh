af2d33bd5c13e36aed3345c592f88e40278072ee
Zachary Light <zacharylight15@mittymonarch.com>
2019-01-10 09:14:18 +0000

    made logging new file work to a seperate directory

--- launchLogging.sh
+++ launchLogging.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ ! -d Logs ]; then
+if [ ! -d ~/BobcatLogs/ ]; then
     echo making Logs dir
-    mkdir -p Logs;
+    mkdir -p ~/BobcatLogs/;
     echo Logs creaed
 fi
-filename=$(date +'Logs/%m.%d.%Y,%H:%M:%S.log')
+filename=$(date +%m.%d.%Y,%H:%M:%S.log)
+echo creating file ~/BobcatLogs/$filename
+touch ~/BobcatLogs/$filename
 echo Lauching classAlert.py
-python -u classAlert.py | tee $filename
+python -u classAlert.py | tee -a ~/BobcatLogs/$filename
