b033afb2e534cac48cc21bd7963f3e0e72696328
Zachary Light <zacharylight15@mittymonarch.com>
2019-01-29 14:27:50 -0800

    threading onionIp in case of inability to connect

--- onionIp.py
+++ onionIp.py
@@ -1,11 +1,15 @@
 import requests
 
 def getOnionIPList():
-    print "getting ip list"
-    myIP = requests.get('https://api.ipify.org').text
-    ipList = requests.get('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip='+myIP+'&port=').text.split('#')
-    a = ipList[len(ipList)-1]
-    f=open("ipList.txt",'w')
-    f.write(a)
-    print "ip list written"
-    f.close()
+    try:
+        print "getting ip list"
+        myIP = requests.get('https://api.ipify.org').text
+        ipList = requests.get('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip='+myIP+'&port=').text.split('#')
+        a = ipList[len(ipList)-1]
+        f=open("ipList.txt",'w')
+        f.write(a)
+        print "ip list written"
+        f.close()
+    except:
+        time.sleep(10)
+        getOnionIPList()
