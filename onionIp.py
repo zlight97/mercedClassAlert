7c16641350d419fe281b2cec51a5b5e76882b2a2
Zachary Light <zacharylight15@mittymonarch.com>
2019-01-29 14:17:39 -0800

    updating logging

--- onionIp.py
+++ onionIp.py
@@ -1,10 +1,12 @@
 import requests
 
 def getOnionIPList():
+    print "getting ip list"
     myIP = requests.get('https://api.ipify.org').text
     ipList = requests.get('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip='+myIP+'&port=').text.split('#')
     a = ipList[len(ipList)-1]
     f=open("ipList.txt",'w')
     f.write(a)
+    print "ip list written"
     f.close()
 getOnionIPList()
